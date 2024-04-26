39e1bb515843aba9f49c15084a7b7c6635482230
Michal Stys <michal.stys@vmlyr.com>
2024-04-26 14:40:26 +0200

    Fix

--- app/test-error-handling/page.tsx
+++ app/test-error-handling/page.tsx
@@ -4,19 +4,21 @@ import ApiClient from "../helpers/apiMock";
 
 export const dynamic = "force-dynamic";
 
-export default async function Shop() {
+export default async function TestPage() {
   try {
     const data = await getApiData();
     return <div>VALID RESPONSE: {data}</div>;
   } catch (e) {
-    let err = e as ApiClientError;
+    if (e instanceof ApiClientError) {
+      let err = e as ApiClientError;
 
-    if (err.httpCode === 403) {
-      return <div>ERROR {`403 - ${err.message}`} </div>;
-    } else if (err.httpCode === 401) {
-      return <div> ERROR 401 - Check access rules</div>;
-    } else if (err.httpCode === 404) {
-      notFound();
+      if (err.httpCode === 403) {
+        return <div>ERROR {`403 - ${err.message}`} </div>;
+      } else if (err.httpCode === 401) {
+        return <div> ERROR 401 - Check access rules</div>;
+      } else if (err.httpCode === 404) {
+        notFound();
+      } else throw e; // unhandled error
     } else throw e; // unhandled error
   }
 }
